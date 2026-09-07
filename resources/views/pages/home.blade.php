da89024789f3bd8bc24308b673be7a0ec8ddf0cb
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-09-07 15:11:06 +0200

    chore: initial commit

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component { };
+?>
+
+<div class="min-w-screen min-h-screen flex items-center justify-center elevation-1">
+    <h1 class="text-7xl font-semibold">Hello world!</h1>
+</div>
